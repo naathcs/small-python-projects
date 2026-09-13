b3b7629caa0ebe5c05247b08dce08888a84847c9
naathcs <naath.cs@outlook.com>
2026-09-13 20:25:47 -0300

    Finish code and details to avoid any crashing. Most were while loops for incorrect input from the user side. Started adding links on README used for reference.

--- 5-currency-converter/5-currency-converter.py
+++ 5-currency-converter/5-currency-converter.py
@@ -3,19 +3,42 @@ from currency_converter import CurrencyConverter
 
 
 c = CurrencyConverter()
+currencies = c.currencies
 
 # Let user choose currency from and to convert
 def get_user_input():
-    print('What is the amount you want to convert? ')
-    amount = float(input())
 
-    print('Choose the currency you want to convert FROM?\n' \
-    '(e.g., USD, CAD, EUR, BRL)')
+    while True:
+        try:
+            print('What is the amount you want to convert? (e.g., 100.00)')
+            amount = float(input())
+
+            if amount == 0:
+                print('Amount cannot be zero. Please try again!')
+                continue # Continue loop if the amount in not valid
+            break # If amount is valid, break loop and go to next code section
+
+        except ValueError:
+            print('Please enter a valid amount!')
+
+
+
+    # Check if currency input is available in library
+
+    print('Choose the currency you want to convert FROM?\n' '(e.g., USD, CAD, EUR, BRL)')
     currencyFrom = input()[:3].upper()
+    while currencyFrom not in currencies: 
+        print('Invalid entry. Please try again')
+        print('Choose the currency you want to convert FROM?\n' '(e.g., USD, CAD, EUR, BRL)')
+        currencyFrom = input()[:3].upper()
 
-    print('Choose the currency you want to convert to?\n' \
-    '(e.g., USD, CAD, EUR, BRL)')
+
+    print('Choose the currency you want to convert TO?\n' '(e.g., USD, CAD, EUR, BRL)')
     currencyTo = input()[:3].upper()
+    while currencyTo not in currencies:
+        print('Invalid entry. Please try again')
+        print('Choose the currency you want to convert TO?\n' '(e.g., USD, CAD, EUR, BRL)')
+        currencyTo = input()[:3].upper()
 
     #return variables
     return amount, currencyFrom, currencyTo
@@ -25,24 +48,23 @@ def get_user_input():
 def currency_conversion():
 
     amount, currencyFrom, currencyTo = get_user_input()
-    
-    # Check currency
-    currencies = c.currencies
-    
-    if currencyFrom in currencies and currencyTo in currencies:
-        conversionResult = c.convert(amount, f'{currencyFrom}', f'{currencyTo}')
-        print(f'Your conversion is: {conversionResult:.2f} {currencyTo}')
-    else:
-        print('One or more entries are not valid')
+
+    conversionResult = c.convert(amount, f'{currencyFrom}', f'{currencyTo}')
+    print(f'Your conversion is: {conversionResult:.2f} {currencyTo}')
 
 
 # Show the Conversion Rate and the date this conversion is from
+
 while True:
+
     currency_conversion()
     print('\nWould you like to run another conversion? (Y/N)')
-    answer = input()[:1].upper()
+    answer = input().strip().upper()[:1]
+
+    while answer not in ('Y','N'):
+        print('\nInvalid Entry. Please try again.')             
+        print('\nWould you like to run another conversion? (Y/N)')
+        answer = input().strip().upper()[:1]
 
     if answer == 'N':
-        break
-    elif answer != 'Y':
-        print('Please enter a valid answer.')
+            break
--- 5-currency-converter/README.md
+++ 5-currency-converter/README.md
@@ -1,5 +1,5 @@
 # QR Code Generator
-Simple Currency Converter program, using an popular currency library.
+Simple Currency Converter program, using a popular currency library.
 
 ## Table of Content 
 - [Overview](#overview)
@@ -25,7 +25,9 @@ Simple Currency Converter program, using an popular currency library.
 
 #### How to Run
 
-Open the terminal and find the folder where the Python file was downloaded, then follow the steps below.
+Follow this steps if you do not have a virtual envrionment or the necessary libraries to run the code.
+
+Open your terminal and find the folder where the Python file was downloaded, then follow the steps below.
 
 ```
 # Create a Virtual Environment
@@ -38,7 +40,7 @@ source .venv/bin/activate
 
 If not installed already, follow this step
 ```
-# (OPTIONAL) Install Currency Converter
+# (OPTIONAL) Install Currency Converter library, if not already installed on you device/virtual environment.
 pip3 install currencyconverter
 ```
 
@@ -46,8 +48,12 @@ pip3 install currencyconverter
 
 - [Currency Converter Library](https://pypi.org/project/CurrencyConverter/) - Library Downloaded with Currency Historical Rates with the referency currency (Euro). 
 - [Currency List](https://currencyapi.net/documentation/currencies/) - Check the list of available currencies for conversion.
+- [Limit Character Amount](https://stackoverflow.com/questions/28465779/how-do-i-limit-the-amount-of-letters-in-a-string) - How to limit the amount of characters in a variable.
+- [Not In Operator](https://docs.python.org/3/reference/expressions.html) - Section 6.10.2 that talks about the `in` and `not in` operators.
+- [Break and Continue Statements](https://docs.python.org/3/reference/simple_stmts.html#the-continue-statement) - `Break` and `Continue` statements to be used on a while loop.
+- [Repeating Tasks with While Loop](https://realpython.com/python-while-loop/) - While Loop for repeating tasks until a condition is met.
 
-## Authorde 
+## Author 
 
 - Developed by Nathalia Santos 🐉<br><br>
 [![LinkedIn Badge](https://img.shields.io/badge/-Nathalia_Santos-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/naathcs/)](https://www.linkedin.com/in/naathcs/)
